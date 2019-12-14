6dcb8cb9159f6134ed4275c67018ebc17aaf7855
Craig <sgccarey@gmail.com>
2019-12-14 13:48:25 +0000

    BinarySearchTree

---
+++ BinarySearchTree.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+
+# TODO: deletion
+
+
+class Node(object):
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+class BinarySearchTree(object):
+    def __init__(self, root):
+        self.root = Node(root)
+
+    def insert(self, new_val):
+        self.insert_helper(self.root, new_val)
+
+    def insert_helper(self, current, new_val):
+
+        if current.value < new_val:
+            if current.right:
+                self.insert_helper(current.right, new_val)
+            else:
+                current.right = Node(new_val)
+        else:
+            if current.left:
+                self.insert_helper(current.left, new_val)
+            else:
+                current.left = Node(new_val)
+
+    def search(self, find_val):
+        return self.search_helper(self.root, find_val)
+
+    def search_helper(self, current, find_val):
+
+        if current is None:
+            return False
+
+        if current.value == find_val:
+            return True
+
+        if current.value < find_val:
+            self.search_helper(current.right, find_val)
+        else:
+            self.search_helper(current.left, find_val)
+
+    def print_tree(self):
+        return self.preorder_print(tree.root, "")[:-1]
+
+    def preorder_print(self, start, traversal):
+        if start:
+            traversal += (str(start.value) + "-")
+            traversal = self.preorder_print(start.left, traversal)
+            traversal = self.preorder_print(start.right, traversal)
+        return traversal
+
+
+if __name__ == "__main__":
+    print("Running {} tests:".format(__file__), end=' ')
+
+    # Set up tree
+    tree = BinarySearchTree(4)
+
+    # Insert elements
+    tree.insert(2)
+    tree.insert(1)
+    tree.insert(3)
+    tree.insert(5)
+
+    assert (tree.print_tree() == '4-2-1-3-5')
+
+    # Check search
+    assert (tree.search(4))
+    assert (not tree.search(6))
