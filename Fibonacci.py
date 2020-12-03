d66cd6530a382768519e30fa24a43f902ebae397
Craig Carey <craig@cynitec.uk>
2020-12-03 21:56:31 +0000

    Dynamic Fibonacci

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,27 +1,45 @@
 #!/usr/bin/env python3
 
+# Dynamic programming is recursion + memoisation
+#
+# It works when the problem features both:
+#
+# Optimal substructure:
+#    a globally optimal solution can be found by combining optimal
+#    solutions to local subproblems
+#    e.g. fib(x) = fib(x-1) + fib(x-2) for x > 1
+#
+# Overlapping subproblems:
+#    finding an optimal solution involves solving the same problem
+#    multiple times
+def dynamic_fib(n, memo):
+    if n == 0 or n == 1:
+        return 1
 
-def get_fib(position):
+    if n in memo:
+        return memo[n]
+
+    result = dynamic_fib(n - 1, memo) + dynamic_fib(n - 2, memo)
+    memo[n] = result
+    return result
+
+
+def recursive_fib(position):
     if position == 0 or position == 1:
-        return position
+        return 1
     else:
-        return get_fib(position - 1) + get_fib(position - 2)
+        return recursive_fib(position - 1) + recursive_fib(position - 2)
 
 
 if __name__ == "__main__":
     print("Running {} tests:".format(__file__), end=' ')
 
-    # Test cases
-    assert (get_fib(0) == 0)
-    assert (get_fib(1) == 1)
-    assert (get_fib(2) == 1)
-    assert (get_fib(3) == 2)
-    assert (get_fib(4) == 3)
-    assert (get_fib(5) == 5)
-    assert (get_fib(6) == 8)
-    assert (get_fib(7) == 13)
-    assert (get_fib(8) == 21)
-    assert (get_fib(9) == 34)
-    assert (get_fib(11) == 89)
+    print("\n", dynamic_fib(120, {}))
+
+    gts = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+
+    for i in range(10):
+        assert (recursive_fib(i) == gts[i])
+        assert (recursive_fib(i) == dynamic_fib(i, {}))
 
     print("Passed")
